eb324b9ccc441c41c754691ff1ca0438b5986f0e
Omair <omairraza9@gmail.com>
2017-10-03 22:05:55 -0400

    first add

---
+++ rooms.rb
@@ -0,0 +1,36 @@
+all = {
+  data:{
+    rooms:[
+     { id: 1, room_number: "201", capacity: 50}, { id: 2, room_number: "301", capacity: 200 }, { id: 3, room_number: "1B", capacity: 100}
+    ],
+    events: [
+      { id: 1, room_id: 2, start_time: 18, end_time: 20, attendees: 75 },
+      { id: 2, room_id: 1, start_time: 10, end_time: 18, attendees: 25 },
+      { id: 3, room_id: 2, start_time: 10, end_time: 18, attendees: 20 },
+      { id: 4, room_id: 3, start_time: 18, end_time: 21, attendees: 56 },
+    ]
+  }
+}
+
+#1 Retrieve the capacity of room 201 and store it in a variable.
+
+  all[:data][:rooms].each do |room|
+      if room.room_number == '201'
+        puts room.capacity
+      end
+  end
+
+
+#Find all the events taking place in room 201. Iterate through them and print "ok" if the number of planned attendees
+#will fit in the room.
+
+all[:data][:events].each do |event|
+  if event.room_id == 1
+    if
+      event.attendees < all.data.rooms[0].capacity
+    end
+    puts "ok"
+  elsif
+    puts "too many people"
+  end
+end
